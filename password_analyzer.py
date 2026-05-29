85f43b08afefa359aaf319a675492aaddd885de3
harinisubha13 <harini.sh1309@gmail.com>
2026-05-29 19:27:02 +0530

    Refactor password strength checks and improve readability

--- password_analyzer.py
+++ password_analyzer.py
@@ -4,31 +4,34 @@ password = input("Enter your password: ")
 
 strength = 0
 
-# Check password length
+# Length check
 if len(password) >= 8:
     strength += 1
 
-# Check uppercase letter
+# Uppercase check
 if any(char.isupper() for char in password):
     strength += 1
 
-# Check lowercase letter
+# Lowercase check
 if any(char.islower() for char in password):
     strength += 1
 
-# Check number
+# Number check
 if any(char.isdigit() for char in password):
     strength += 1
 
-# Check special character
-special_chars = "!@#$%^&*()_+-="
-if any(char in special_chars for char in password):
+# Special character check
+special = "!@#$%^&*"
+
+if any(char in special for char in password):
     strength += 1
 
-# Result
+# Final result
 if strength <= 2:
     print("Weak Password")
-elif strength == 3 or strength == 4:
+
+elif strength <= 4:
     print("Medium Password")
+
 else:
     print("Strong Password")
